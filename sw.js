3bf21dac064e4c4dbf91e78cc0d06fcde62a6c03
sikumel-ux <sikumel@gmail.com>
2026-02-09 22:21:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
+const CACHE_NAME = 'doodle-math-v2'; // <--- Ganti angkanya setiap update (v2, v3, dst)
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Pasang Service Worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('math-store').then((cache) => cache.addAll([
-      'index.html',
-      'manifest.json'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+  self.skipWaiting(); // Paksa SW baru aktif langsung
+});
+
+// Hapus Cache Lama
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
